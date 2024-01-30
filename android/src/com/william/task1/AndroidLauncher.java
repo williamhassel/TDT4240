7261615e721d5af18bed76357f90698c32da54aa
William <william.r.hassel@gmail.com>
2024-01-30 20:05:57 +0100

    setup

---
+++ android/src/com/william/task1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.william.task1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.william.task1.task1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new task1(), config);
+	}
+}
---
+++ desktop/src/com/william/task1/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.william.task1;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.william.task1.task1;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("task1");
+		new Lwjgl3Application(new task1(), config);
+	}
+}
